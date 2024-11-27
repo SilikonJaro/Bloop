018a4fce81a85c9452c99001cf45f3734c52adbb
SilikonJaro <jaros124@trebesin.cz>
2024-11-27 09:37:45 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
 
 let score = 0;
 
+let clickCount = 1;
+let clickMulti = 1;
+
 function Click() {
-    score = score + 1;
+    score = score + (clickCount * clickMulti);
     console.log(score);
 }
