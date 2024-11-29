479b5c4f5b274cff2cd8035be4a0a3aa4998c423
HJ <jaros124@trebesin.cz>
2024-11-29 11:10:58 +0100

    verze 1

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,33 @@ let score = 0;
 
 let clickCount = 1;
 let clickMulti = 1;
+const CountPrise = 100;
+const MultiPrise = 1000;
+
 
 function Click() {
     score = score + (clickCount * clickMulti);
     console.log(score);
+}
+
+function BuyCount() {
+    if (score >= CountPrise ) {
+        console.log("buy cound for 10");
+        score = score - CountPrise;
+        clickCount = clickCount + 1;
+    } else {
+        console.log("dont have money");
+    }
+}
+
+function BuyMulti() {
+    if (score >= MultiPrise ) {
+        console.log("buy multi for 100");
+        score = score - MultiPrise;
+        clickCount = clickCount + 1;
+    } else {
+        console.log("dont have money");
+    }
 }
 
 function PlayMusic() {
