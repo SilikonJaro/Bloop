7fa6e2a15c785bd6943a3a28f24ee0b18c3b193d
Saturuu <140846158+Saturuu@users.noreply.github.com>
2024-12-03 07:20:56 +0100

    Money Display

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,59 @@
+///////////////////////
+////// Variables //////
+///////////////////////
 
 let score = 0;
 
 let clickCount = 1;
 let clickMulti = 1;
-const CountPrise = 100;
-const MultiPrise = 1000;
+const CountPrice = 100;
+const MultiPrice = 1000;
 
+const audio = new Audio("./SOUNDS/ReadyPixelOne.mp3");
+const ScoreText = document.getElementById("Score")
+
+///////////////////////
+////// Functions //////
+///////////////////////
+
+function PlayMusic() {
+    let audio = new Audio("./SOUNDS/ReadyPixelOne.mp3");
+    audio.play();
+}
+
+/// Buttons functions ///
 
 function Click() {
     score = score + (clickCount * clickMulti);
     console.log(score);
+    UpdateScore();
+    PlayMusic();
 }
 
 function BuyCount() {
-    if (score >= CountPrise ) {
+    if (score >= CountPrice ) {
         console.log("buy cound for 100");
-        score = score - CountPrise;
+        score = score - CountPrice;
         clickCount = clickCount + 1;
+        UpdateScore();
     } else {
         console.log("dont have money");
     }
 }
 
 function BuyMulti() {
-    if (score >= MultiPrise ) {
+    if (score >= MultiPrice ) {
         console.log("buy multi for 1000");
-        score = score - MultiPrise;
+        score = score - MultiPrice;
         clickCount = clickCount + 1;
+        UpdateScore();
     } else {
         console.log("dont have money");
     }
 }
 
-function PlayMusic() {
-    let audio = new Audio("./SOUNDS/ReadyPixelOne.mp3");
-    audio.play();
+/// Update text ///
+
+function UpdateScore() {
+    ScoreText.innerHTML = "Money " + score;
 }
